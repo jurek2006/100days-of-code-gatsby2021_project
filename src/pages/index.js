d85940d6e021d474915a3a2744def2893fdf8528
Jurek <jurek2006@gmail.com>
2021-01-26 09:57:14 +0100

    chore: Wes Bos' configuration of ESLint and Prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
-import * as React from "react"
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import * as React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-const IndexPage = () => <Layout>
-  <SEO title="Index" />
-  <p>AudioC0RE PoC of website. Content comes from Contentful.</p>
-  <p>All pages are generated with data from Contentful with Gatsby’s File System Route API  </p>
-  <p>I don't know yet how to redirect the main index page to /home as the latter is generated with Gatsby’s File System Route API</p>
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Index" />
+    <p>AudioC0RE PoC of website. Content comes from Contentful.</p>
+    <p>
+      All pages are generated with data from Contentful with Gatsby’s File
+      System Route API{' '}
+    </p>
+    <p>
+      I don't know yet how to redirect the main index page to /home as the
+      latter is generated with Gatsby’s File System Route API
+    </p>
+  </Layout>
+);
 
-</Layout> 
-
-
-export default IndexPage
+export default IndexPage;
