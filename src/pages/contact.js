0f284b4a8e4124e83354860acc2fee068d319195
Jurek <jurek2006@gmail.com>
2021-02-01 13:59:57 +0100

    feat: contact form with formium
    Styled a bit with Chakra-ui

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,27 +1,60 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
-import { FormiumForm } from '@formium/react';
+import { FormiumForm, defaultComponents } from '@formium/react';
+import {
+  FormControl as ChakraFormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Textarea,
+  Input as TextInput,
+  Button as SubmitButton,
+  Container,
+} from '@chakra-ui/react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import { formium } from '../lib/formium';
+
+const FormControl = ({ label, children, required, error, description }) => (
+  <ChakraFormControl isRequired={required} isInvalid={error}>
+    <FormLabel>{label}</FormLabel>
+    {children}
+    <FormErrorMessage>{error}</FormErrorMessage>
+    <FormHelperText>{description}</FormHelperText>
+  </ChakraFormControl>
+);
+
+// Always define these outside of React so that
+// components are not needlessly recreated on each render
+const myComponents = {
+  ...defaultComponents,
+  FormControl,
+  FormLabel,
+  TextInput,
+  Textarea,
+  SubmitButton,
+};
 
 const Contact = ({ data }) => {
   const [success, setSuccess] = React.useState(false);
 
   return (
-    <Layout>
+    <Layout title="Contact us">
       <SEO title="Contact us" />
       {success ? (
-        <div>Thank you! Your response has been recorded.</div>
+        <Container>Thank you! Your response has been recorded.</Container>
       ) : (
-        <FormiumForm
-          data={data.formiumForm}
-          onSubmit={async (values) => {
-            // Send form values to Formium
-            await formium.submitForm('contactus', values);
-            setSuccess(true);
-          }}
-        />
+        <Container>
+          <FormiumForm
+            data={data.formiumForm}
+            components={myComponents}
+            onSubmit={async (values) => {
+              // Send form values to Formium
+              await formium.submitForm('contactus', values);
+              setSuccess(true);
+            }}
+          />
+        </Container>
       )}
     </Layout>
   );
